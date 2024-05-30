18ff79020613a1288b7cf33d58df9b6e3b522dbe
yiyc-kor <yiyc.kor@gmail.com>
2024-05-30 20:46:47 +0900

    Edit app.py: Fixed Bug in EN2KO. Developed Function for Logging Input&Output

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ import torch
 from kiwipiepy import Kiwi
 kiwi = Kiwi()
 import nltk
+import os
 
 app = Flask(__name__)
 
 # 모델 디렉토리 설정
 model_dir_ko = "ko_en_checkpoint"
-model_dir_en = "en_ko_checkpoint-56250"
+model_dir_en = "en_ko_checkpoint-37500"
 
 # 한국어 및 영어 토크나이저 및 모델 로드
 tokenizer_ko = AutoTokenizer.from_pretrained(model_dir_ko, use_fast=False)
@@ -20,6 +21,41 @@ model_en = AutoModelForSeq2SeqLM.from_pretrained(model_dir_en)
 # 파일 경로 설정
 original_texts_file_ko = 'original_texts_ko_en.tsv'
 translations_file_ko = 'translations_ko_en.tsv'
+original_texts_file_en = 'original_texts_en_ko.tsv'
+translations_file_en = 'translations_en_ko.tsv'
+
+def initialize_files():
+    # 원문 텍스트 파일 초기화
+    if not os.path.exists(original_texts_file_ko):
+        with open(original_texts_file_ko, 'w', encoding='utf-8') as f:
+            f.write("ko\ten\n")
+    if not os.path.exists(original_texts_file_en):
+        with open(original_texts_file_en, 'w', encoding='utf-8') as f:
+            f.write("en\tko\n")
+
+    # 번역 파일 초기화
+    if not os.path.exists(translations_file_ko):
+        with open(translations_file_ko, 'w', encoding='utf-8') as f:
+            f.write("ko\ten\n")
+    if not os.path.exists(translations_file_en):
+        with open(translations_file_en, 'w', encoding='utf-8') as f:
+            f.write("en\tko\n")
+
+def save_original_text(source_text:str, translated_text:str, language:str):
+    if language == 'ko':
+        with open(original_texts_file_ko, 'a', encoding='utf-8') as f:
+            f.write(f"{source_text}\t{translated_text}\n")
+    elif language == 'en':
+        with open(original_texts_file_en, 'a', encoding='utf-8') as f:
+            f.write(f"{source_text}\t{translated_text}\n")
+
+def save_translation(source_text:str, translated_text:str, language:str):
+    if language == 'ko':
+        with open(translations_file_ko, 'a', encoding='utf-8') as f:
+            f.write(f"{source_text}\t{translated_text}\n")
+    elif language == 'en':
+        with open(translations_file_en, 'a', encoding='utf-8') as f:
+            f.write(f"{source_text}\t{translated_text}\n")
 
 def get_split_text(request):
     # TODO: apply logging
@@ -42,7 +78,6 @@ def translate_en():
     splited_sentence_list = []
     for sentence in sentence_list:
         splited_list = nltk.sent_tokenize(sentence)
-        splited_list = [sentence[0] for sentence in splited_list]
         # print("splited_list:", splited_list)
         splited_sentence_list.extend(splited_list)
     
@@ -53,9 +88,12 @@ def translate_en():
         tokens = tokenizer_en.encode(sentence, return_tensors="pt", max_length=64, truncation=True)
         output = model_en.generate(tokens, max_length=64)
         translated_sentence = tokenizer_en.decode(output[0], skip_special_tokens=True)
+        print(sentence)
+        save_translation(sentence, translated_sentence, 'en')
         translated_list.append(translated_sentence)
     
     print(translated_list)
+    save_original_text(' '.join(sentence_list), ' '.join(translated_list), 'en')
     return jsonify({"translated_text": " ".join(translated_list)})
 
 @app.route('/translate/ko', methods=['POST'])
@@ -77,11 +115,14 @@ def translate_ko():
         tokens = tokenizer_ko.encode(sentence, return_tensors="pt", max_length=64, truncation=True)
         output = model_ko.generate(tokens, max_length=64)
         translated_sentence = tokenizer_ko.decode(output[0], skip_special_tokens=True)
+        save_translation(sentence, translated_sentence, 'ko')
         translated_list.append(translated_sentence)
         # print(translated_sentence)
     
     print(translated_list)
+    save_original_text(' '.join(sentence_list), ' '.join(translated_list), 'ko')
     return jsonify({"translated_text": " ".join(translated_list)})
 
 if __name__ == '__main__':
+    initialize_files()
     app.run(host='0.0.0.0', port=5000)
